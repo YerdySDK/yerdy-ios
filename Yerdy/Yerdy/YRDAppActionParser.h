29903d9122aeeb0183e0089523cbf029b273b31b
Darren Clark <djcdude@gmail.com>
2014-02-13 22:51:06 -0700

    stubbed out YRDAppActionParser

---
+++ Yerdy/Yerdy/YRDAppActionParser.h
@@ -0,0 +1,28 @@
+//
+x//  YRDAppActionParser.h
+//  Yerdy
+//
+//  Created by Darren Clark on 2014-02-13.
+//  Copyright (c) 2014 Yerdy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Parses an app action into something we can use inside the app
+
+
+typedef enum YRDAppActionType {
+	YRDAppActionTypeReward,
+	YRDAppActionTypeInAppPurchase,
+	YRDAppActionTypeItemPurchase,
+} YRDAppActionType;
+
+
+@interface YRDAppActionParser : NSObject
+
+// Returns nil on failure
+- (id)initWithAppAction:(NSString *)appAction;
+
+@property (nonatomic, readonly) YRDAppActionType actionType;
+
+@end
---
+++ Yerdy/Yerdy/YRDAppActionParser.m
@@ -0,0 +1,19 @@
+//
+//  YRDAppActionParser.m
+//  Yerdy
+//
+//  Created by Darren Clark on 2014-02-13.
+//  Copyright (c) 2014 Yerdy. All rights reserved.
+//
+
+#import "YRDAppActionParser.h"
+
+@implementation YRDAppActionParser
+
+- (id)initWithAppAction:(NSString *)appAction
+{
+	// TODO: Implement
+	return nil;
+}
+
+@end

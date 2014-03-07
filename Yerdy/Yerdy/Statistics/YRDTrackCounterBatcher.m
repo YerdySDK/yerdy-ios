16af55a18f0d996c3391427dbb7a6c085be6082e
Darren Clark <djcdude@gmail.com>
2014-03-06 23:28:54 -0700

    sending batched trackCounter events every 5 minutes now

--- Yerdy/Yerdy/Statistics/YRDTrackCounterBatcher.m
+++ Yerdy/Yerdy/Statistics/YRDTrackCounterBatcher.m
@@ -10,6 +10,17 @@
 #import "YRDCounterEvent.h"
 #import "YRDLog.h"
 
+#import "YRDTimeTracker.h"
+#import "YRDTrackCounterRequest.h"
+#import "YRDTrackCounterResponse.h"
+
+#import "YRDURLConnection.h"
+
+
+// Upload every X minutes
+static const int SEND_INTERVAL_MINUTES = 5;
+
+
 @interface YRDTrackCounterBatcher ()
 {
 	// Maps event name -> array of YRDCounterEvents, separated by type (custom, time, player)
@@ -32,7 +43,23 @@
 	_timeEvents = [NSMutableDictionary dictionary];
 	_playerEvents = [NSMutableDictionary dictionary];
 	
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(minutePassedNotification:)
+												 name:YRDTimeTrackerMinutePassedNotification object:nil];
+	
 	return self;
+}
+
+- (void)dealloc
+{
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)minutePassedNotification:(NSNotification *)notification
+{
+	int minutes = [notification.userInfo[YRDTimeTrackerMinutesPassedKey] intValue];
+	if (minutes % SEND_INTERVAL_MINUTES == 0) {
+		[self flush];
+	}
 }
 
 - (void)addEvent:(YRDCounterEvent *)event
@@ -84,6 +111,27 @@
 	// We should only add this event if we *haven't* removed all parameters
 	if (event.parameterNames.count > 0)
 		[events addObject:event];
+}
+
+- (void)flush
+{
+	NSMutableArray *allEvents = [NSMutableArray array];
+	[allEvents addObjectsFromArray:_customEvents.allValues];
+	[allEvents addObjectsFromArray:_timeEvents.allValues];
+	[allEvents addObjectsFromArray:_playerEvents.allValues];
+	
+	// eventGroup is an array of YRDCounterEvents with the same type & name
+	for (NSArray *eventGroup in allEvents) {
+		YRDTrackCounterRequest *request = [YRDTrackCounterRequest requestWithCounterEvents:eventGroup];
+		YRDInfo(@"trackCounter - %@", request.queryParameters);
+		[YRDURLConnection sendRequest:request completionHandler:^(YRDTrackCounterResponse *response, NSError *error) {
+			// do nothing for now...
+		}];
+	}
+	
+	[_customEvents removeAllObjects];
+	[_timeEvents removeAllObjects];
+	[_playerEvents removeAllObjects];
 }
 
 @end
